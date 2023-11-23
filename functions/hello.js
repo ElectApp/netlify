aa7cbc6371111f03468d4067c9fe0452c7602119
Elect <somsak.elect.new@gmail.com>
2023-11-23 18:44:28 +0700

    http post

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Use body-parser middleware to parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/.netlify/functions/hello', (req, res) => {
   res.json({ message: 'Hello, World!' });
+});
+
+app.post('/pay-confirm', (req, res) => {
+    let raw = req.body;
+    res.json({ message: raw });
+
+    res.send('Data received successfully');
 });
 
 module.exports.handler = serverless(app);
