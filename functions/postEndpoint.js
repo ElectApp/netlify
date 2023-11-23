e5357ebd4de3e10d44dd5970d931f0b4a170bfb2
Elect <somsak.elect.new@gmail.com>
2023-11-23 19:06:14 +0700

    insert log

--- functions/postEndpoint.js
+++ functions/postEndpoint.js
@@ -3,7 +3,7 @@ exports.handler = async function (event, context) {
     try {
       // Parse JSON data from the request body
       const requestData = JSON.parse(event.body);
-      console.log("got data: ", event.body);
+      console.log("got data:", event.body);
   
       // Perform actions with the received data
       // ...
@@ -13,6 +13,7 @@ exports.handler = async function (event, context) {
         body: JSON.stringify({ message: 'POST request received successfully', data: requestData }),
       };
     } catch (error) {
+        console.error("error:", error.message);
       return {
         statusCode: 500,
         body: JSON.stringify({ message: 'Internal Server Error', error: error.message }),
